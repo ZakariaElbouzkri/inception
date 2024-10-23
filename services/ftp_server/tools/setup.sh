757c96faa809aa3a5830605a105bbca6b33cf25b
ZakariaElbouzkri <zel-bouz@student.1337.ma>
2024-10-23 05:47:01 +0100

    chore: Add FTP server container and configuration

--- services/ftp_server/tools/setup.sh
+++ services/ftp_server/tools/setup.sh
@@ -1,13 +1,13 @@
 if ! id $FTP_USER &>/dev/null; then
-    useradd -m $FTP_USER
-
+    useradd -m -u 1001 $FTP_USER
     echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
-    chmod 755 /srv/ftp
-
-    chown -R $FTP_USER:$FTP_USER /srv/ftp
-    chown -R $FTP_USER:$FTP_USER /var/run/vsftpd/empty
-
-    chmod 755 /var/run/vsftpd
 fi
 
+# Always ensure correct permissions for the FTP directories
+chmod 755 /srv/ftp
+chown -R $FTP_USER:$FTP_USER /srv/ftp
+chown -R $FTP_USER:$FTP_USER /var/run/vsftpd/empty
+chmod 755 /var/run/vsftpd
+
+# Start the FTP service
 exec $@
