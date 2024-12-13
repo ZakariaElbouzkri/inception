b48bcaa461fd8c98d143f2b4609dc94fbd1150b3
Zakaria El bouzkri <elbouzkri9@gmail.com>
2024-12-13 09:07:14 +0100

    chore: Add make_env.py script for generating environment variables and update .gitignore to exclude .venv/

---
+++ tools/make_env.py
@@ -0,0 +1,21 @@
+import faker
+import random
+
+f = faker.Faker()
+
+env = {
+    "MYSQL_PASSWORD": f.password(special_chars=False),
+    "MYSQL_ROOT_PASSWORD": f.password(special_chars=False),
+    "MYSQL_DATABASE": f.word(),
+    "MYSQL_USER": f.user_name(),
+    "WP_ADMIN_EMAIL": f.email(),
+    "WP_ADMIN_USER": f.user_name(),
+    "WP_ADMIN_PASS": f.password(special_chars=False),
+    "WP_USER_USERNAME": f.user_name(),
+    "WP_USER_EMAIL": f.email(),
+    "WP_USER_ROLE": random.choice(["author", "contributor", "subscriber"]),
+    "WP_USER_PASS": f.password(special_chars=False),
+}
+
+for key, value in env.items():
+    print(f"{key}=\"{value}\"")
