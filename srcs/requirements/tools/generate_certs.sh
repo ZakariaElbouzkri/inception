5bf61a1ca05d8458d4585f84742661a854642072
Zakaria El bouzkri <elbouzkri9@gmail.com>
2024-12-13 12:40:15 +0100

    chore: Clean up comments and formatting in certificate generation script

--- srcs/requirements/tools/generate_certs.sh
+++ srcs/requirements/tools/generate_certs.sh
@@ -9,12 +9,10 @@ output_dir="certs"
 # Create the output directory if it doesn't exist
 mkdir -p "$output_dir"
 
-
-# # Use OpenSSL to generate the self-signed certificate with additional subject information (organization and location)
+# Use OpenSSL to generate the self-signed certificate with additional subject information (organization and location)
 openssl req -x509 -nodes -newkey rsa:2048 \
     -keyout "$output_dir/${domains[0]}.key" \
     -out "$output_dir/${domains[0]}.crt" -days 365 \
     -subj "/CN=${domains[0]}/O=1337/L=Morocco/C=MA/ST=Agadir" \
     -extensions SAN \
-    -config <(echo -e "[req]\ndistinguished_name=dn\n[dn]\n[SAN]\nsubjectAltName=$(printf "DNS:%s," "${domains[@]}" | sed 's/,$//'))
-
+    -config <(echo -e "[req]\ndistinguished_name=dn\n[dn]\n[SAN]\nsubjectAltName=$(printf "DNS:%s," "${domains[@]}" | sed 's/,$//')")
