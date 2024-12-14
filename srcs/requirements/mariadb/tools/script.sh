3e6a3a73e307520f5a924dd05f89c1f0ac23d94e
ZakariaElbouzkri <elbouzkri9@gmail.com>
2024-12-14 12:18:03 +0100

    chore: Update MariaDB Dockerfile to use Debian Buster and improve setup script

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# # !/bin/bash
 set -e
-# Start MariaDB service
+
 service mysql start
 
 sleep 5
-# If first run, initialize the database
-if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
-  # mysql_install_db --user=mysql > /dev/null
 
-  # Create user and database if they don't exist, using root password
+if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
   mysql  <<-EOSQL
     CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
     CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
@@ -18,11 +14,9 @@ if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
     ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 EOSQL
 
-  # Secure installation by setting root password
   mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 fi
 
 mysqladmin -uroot -p$MYSQL_ROOT_PASSWORD shutdown
 
-# Keep MySQL alive
-exec "$@"
+exec mysqld
